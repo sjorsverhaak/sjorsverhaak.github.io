0921f97a7f85471c8302ca75419f364db171287d
sjorsverhaak <sjorsverhaak@gmail.com>
2025-08-15 18:27:35 +0000

    Deploying to gh-pages from @ sjorsverhaak/sjorsverhaak.github.io@7d73c796da4d8f66c93b87ff6d178bd353e62b6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-posts",
+          title: "posts",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{
       id: 'light-theme',
       title: 'Change theme to light',
